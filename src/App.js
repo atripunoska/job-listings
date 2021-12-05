ad7592d8d9696fd2446188a0f801f98c9860ddd0
atripunoska <atripunoska@gmail.com>
2021-12-05 21:12:42 +0100

    Add Job listing data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
 import "./styles/main.scss";
+import data from "./data";
 
 function App() {
+  const Cards = () => {
+    const cardItems = data.map((d) => (
+      <Card key={d.id} company={d.company}></Card>
+    ));
+    return cardItems;
+  };
   return (
     <div className="App">
       <Header />
       <main>
         <Card company="test" />
+        {Cards()}
       </main>
     </div>
   );
