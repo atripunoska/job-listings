b72124a52dfd92dd922ea5e0355c0c11b69653d7
atripunoska <atripunoska@gmail.com>
2021-12-07 18:43:29 +0100

    Fixes for data representation

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import data from "./data";
 function App() {
   const Cards = () => {
     const cardItems = data.map((d) => (
-      <Card key={d.id} company={d.company}></Card>
+      <Card
+        key={d.id}
+        company={d.company}
+        logo={d.logo}
+        location={d.location}
+        position={d.position}
+        postedAt={d.postedAt}
+        role={d.role}
+        contract={d.contract}
+        filters={d.languages}
+      ></Card>
     ));
     return cardItems;
   };
   return (
     <div className="App">
       <Header />
-      <main>
-        <Card company="test" />
-        {Cards()}
-      </main>
+      <main>{Cards()}</main>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
 const Card = (props) => {
+  const Languages = () => {
+    const filters = props.filters;
+    let lang = "";
+    if (filters) {
+      lang = (
+        <div>
+          <ul>
+            {filters.map((filter) => (
+              <li key={filter}> {filter} </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    return lang;
+  };
   return (
     <div className="card">
-      <div className="card__logo"></div>
+      <div className="card__logo">
+        <img src={props.logo} alt="" />
+      </div>
       <div className="card__company">{props.company}</div>
-      <div className="card__position"></div>
+      <div className="card__position">{props.position}</div>
+      <div className="card__role">{props.role}</div>
+      <div className="card__level">{props.level}</div>
+      <div className="card__postedAt">{props.postedAt}</div>
+      <div className="card__contract">{props.contract}</div>
+      <div className="card__location">{props.location}</div>
+      <div className="card__filters">{Languages()}</div>
     </div>
   );
 };
