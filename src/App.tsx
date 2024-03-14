ab8e23aab8e9c7ab0712db16c9e397e754430e2a
atripunoska <atripunoska@gmail.com>
2024-03-14 17:57:37 +0100

    refactor: update url in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <>
-      <header className="h-[150px] md:h-[200px] bg-[url('./src/assets/bg-header-mobile.svg')] md:bg-[url('./src/assets/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover bg-dark-cyan"></header>
+      <header className="h-[150px] md:h-[200px] bg-[url('/bg-header-mobile.svg')] md:bg-[url('/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover bg-dark-cyan"></header>
 
       <main className="bg-light-grayish-cyan relative pt-5">
         {activeFilters.length > 0 && (
