f358d590c1c6ed3d9798a47cce5de125787b4541
atripunoska <atripunoska@gmail.com>
2024-03-14 16:54:12 +0100

    feat: implement logic for filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,135 @@
-import data from "../data.json";
+import { useState } from "react";
 import JobListingCard from "./components/JobListingCard";
+import data from "../data.json";
 
 function App() {
+  const [activeFilters, setActiveFilters] = useState<string[]>([]);
+
+  function handleFilterClick(item: string) {
+    if (!activeFilters.includes(item)) {
+      const filtersCopy = [...activeFilters, item];
+      setActiveFilters(filtersCopy);
+    }
+    return;
+  }
+
+  function handleFilterDelete(item: string) {
+    const updatedFilters = activeFilters.filter((filter) => {
+      return filter !== item;
+    });
+    setActiveFilters(updatedFilters);
+  }
+
+  function deleteAllFilters() {
+    setActiveFilters([]);
+  }
+
   return (
     <>
       <header className="h-[150px] md:h-[200px] bg-[url('./src/assets/bg-header-mobile.svg')] md:bg-[url('./src/assets/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover bg-dark-cyan"></header>
-      <main className="bg-light-grayish-cyan">
-        <div className="w-10/12 mx-auto py-12">
-          {data.map((d) => (
-            <JobListingCard
-              key={d.id}
-              company={d.company}
-              logo={d.logo}
-              new={d.new}
-              featured={d.featured}
-              position={d.position}
-              role={d.role}
-              level={d.level}
-              postedAt={d.postedAt}
-              contract={d.contract}
-              location={d.location}
-              languages={d.languages}
-              tools={d.tools}
-            />
-          ))}
+
+      <main className="bg-light-grayish-cyan relative pt-5">
+        {activeFilters.length > 0 && (
+          <div className="bg-white p-4 md:p-6 rounded  flex justify-between shadow-md items-center  w-10/12 mx-auto relative -top-12 left-0 right-0 ">
+            <div className="flex flex-wrap">
+              {activeFilters.map((filter) => (
+                <div
+                  key={filter}
+                  className="bg-light-grayish-cyan text-dark-cyan mr-3 rounded-tl mb-3
+                  rounded-bl"
+                >
+                  <span className="px-2 font-bold">{filter}</span>
+                  <button
+                    onClick={() => handleFilterDelete(filter)}
+                    className="bg-dark-cyan text-white size-7 uppercase hover:bg-very-dark-gray-cyan rounded-tr
+                    rounded-br aspect-square font-bold inline-flex justify-center items-center"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="13"
+                      height="13"
+                      viewBox="0,0,256,256"
+                    >
+                      <g
+                        fill="#ffffff"
+                        fill-rule="nonzero"
+                        stroke="none"
+                        stroke-width="none"
+                        stroke-linecap="butt"
+                        stroke-linejoin="none"
+                        stroke-miterlimit="10"
+                        stroke-dasharray=""
+                        stroke-dashoffset="0"
+                        font-family="none"
+                        font-weight="none"
+                        font-size="none"
+                        text-anchor="none"
+                        // style="mix-blend-mode: normal"
+                      >
+                        <path
+                          transform="scale(5.12,5.12)"
+                          d="M25,22.15625l15.84375,-15.84375l2.84375,2.84375l-15.84375,15.84375l15.9375,15.9375l-2.84375,2.84375l-15.9375,-15.9375l-15.96875,15.9375l-2.8125,-2.8125l15.9375,-15.96875l-15.84375,-15.84375l2.84375,-2.84375z"
+                          id="strokeMainSVG"
+                          stroke="#ffffff"
+                          stroke-width="6"
+                          stroke-linejoin="round"
+                        ></path>
+                        <g
+                          transform="scale(5.12,5.12)"
+                          stroke="none"
+                          stroke-width="1"
+                          stroke-linejoin="miter"
+                        >
+                          <path d="M9.15625,6.3125l-2.84375,2.84375l15.84375,15.84375l-15.9375,15.96875l2.8125,2.8125l15.96875,-15.9375l15.9375,15.9375l2.84375,-2.84375l-15.9375,-15.9375l15.84375,-15.84375l-2.84375,-2.84375l-15.84375,15.84375z"></path>
+                        </g>
+                      </g>
+                    </svg>
+                  </button>
+                </div>
+              ))}
+            </div>
+            {activeFilters.length > 0 && (
+              <button
+                onClick={deleteAllFilters}
+                className="text-dark-cyan font-bold text-lg hover:underline "
+              >
+                Clear
+              </button>
+            )}
+          </div>
+        )}
+
+        <div className="w-10/12 mx-auto py-12 pt-6">
+          {data.map((d) => {
+            if (
+              activeFilters.length === 0 ||
+              d.tools.some((v) => activeFilters.includes(v)) ||
+              d.languages.some((v) => activeFilters.includes(v)) ||
+              activeFilters.includes(d.level) ||
+              activeFilters.includes(d.role)
+            ) {
+              return (
+                <JobListingCard
+                  key={d.id}
+                  company={d.company}
+                  logo={d.logo}
+                  new={d.new}
+                  featured={d.featured}
+                  position={d.position}
+                  role={d.role}
+                  level={d.level}
+                  postedAt={d.postedAt}
+                  contract={d.contract}
+                  location={d.location}
+                  languages={d.languages}
+                  tools={d.tools}
+                  selectedFilters={handleFilterClick}
+                />
+              );
+            }
+          })}
         </div>
       </main>
     </>
--- src/components/JobListingCard.tsx
+++ src/components/JobListingCard.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface JobListingCardProperties {
   company: string;
   logo: string;
@@ -13,77 +11,95 @@ interface JobListingCardProperties {
   location: string;
   languages: string[];
   tools: string[];
+  selectedFilters: (item: string) => void;
 }
+
 const JobListingCard = ({ ...props }: JobListingCardProperties) => {
   return (
-    <div
-      className={`bg-white pt-11 pl-6 pb-6 pr-6 md:p-6 rounded mb-9 md:mb-4 flex flex-col md:flex-row justify-between shadow-md md:items-center items-start ${
-        props.featured ? "border-l-4 border-dark-cyan" : ""
-      }`}
-    >
-      <div className="flex gap-3 relative flex-col md:flex-row">
-        <img
-          src={props.logo}
-          alt={props.company}
-          className="absolute top-[-4.5rem] md:top-0 md:relative size-16 md:size-auto"
-        />
-        <div className="flex flex-col justify-between">
-          <div>
-            <span className="text-dark-cyan font-bold">{props.company}</span>
-            {props.new && (
-              <span className="font-bold bg-dark-cyan rounded-full px-2 py-1.5 text-xs text-white inline-block mx-2 uppercase">
-                New!
+    <>
+      <div
+        className={`bg-white pt-11 pl-6 pb-6 pr-6 md:p-6 rounded mb-9 md:mb-4 flex flex-col md:flex-row justify-between shadow-xl  md:items-center items-start ${
+          props.featured ? "border-l-4 border-dark-cyan" : ""
+        }`}
+      >
+        <div className="flex gap-3 relative flex-col md:flex-row">
+          <img
+            src={props.logo}
+            alt={props.company}
+            className="absolute top-[-4.5rem] md:top-0 md:relative size-16 md:size-auto"
+          />
+          <div className="flex flex-col justify-between">
+            <div>
+              <span className="text-dark-cyan font-bold">{props.company}</span>
+              {props.new && (
+                <span className="font-bold bg-dark-cyan rounded-full px-2 py-1.5 text-xs text-white inline-block mx-2 uppercase">
+                  New!
+                </span>
+              )}
+              {props.featured && (
+                <span className="font-bold font-body bg-very-dark-gray-cyan rounded-full px-2 py-1.5 text-xs text-white inline-block uppercase">
+                  Featured
+                </span>
+              )}
+            </div>
+            <div className="font-bold text-lg hover:text-dark-cyan cursor-pointer">
+              {props.position}
+            </div>
+            <div className="flex items-center">
+              <span className="font-medium text-dark-gray-cyan">
+                {props.postedAt}
               </span>
-            )}
-            {props.featured && (
-              <span className="font-bold font-body bg-very-dark-gray-cyan rounded-full px-2 py-1.5 text-xs text-white inline-block uppercase">
-                Featured
+              <span className="size-1 bg-dark-gray-cyan rounded-full mx-2 inline-block"></span>
+              <span className="font-medium text-dark-gray-cyan">
+                {props.contract}
               </span>
-            )}
-          </div>
-          <div className="font-bold text-lg hover:text-dark-cyan cursor-pointer">
-            {props.position}
-          </div>
-          <div className="flex items-center">
-            <span className="font-medium text-dark-gray-cyan">
-              {props.postedAt}
-            </span>
-            <span className="size-1 bg-dark-gray-cyan rounded-full mx-2 inline-block"></span>
-            <span className="font-medium text-dark-gray-cyan">
-              {props.contract}
-            </span>
-            <span className="size-1 bg-dark-gray-cyan rounded-full mx-2 inline-block"></span>
-            <span className="font-medium text-dark-gray-cyan">
-              {props.location}
-            </span>
+              <span className="size-1 bg-dark-gray-cyan rounded-full mx-2 inline-block"></span>
+              <span className="font-medium text-dark-gray-cyan">
+                {props.location}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full md:w-auto">
-        <div className="my-3 md:hidden  block border-1 bg-very-dark-gray-cyan h-px" />
-        {props.tools?.map((tool) => (
+        <div className="w-full md:w-auto">
+          <div className="my-3 md:hidden  block border-1 bg-very-dark-gray-cyan h-px" />
+
           <button
             className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 hover:bg-dark-cyan hover:text-white"
-            key={tool}
+            onClick={() => props.selectedFilters(props.role)}
           >
-            {tool}
+            {props.role}
           </button>
-        ))}
 
-        {props.languages.map((l) => (
+          {props.tools?.map((tool) => (
+            <button
+              className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 hover:bg-dark-cyan hover:text-white"
+              onClick={() => props.selectedFilters(tool)}
+              key={tool + Math.random()}
+              value={tool}
+            >
+              {tool}
+            </button>
+          ))}
+
+          {props.languages.map((l) => (
+            <button
+              className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 my-2 hover:bg-dark-cyan hover:text-white"
+              key={l + Math.random()}
+              onClick={() => props.selectedFilters(l)}
+            >
+              {l}
+            </button>
+          ))}
+
           <button
-            className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 my-2 hover:bg-dark-cyan hover:text-white"
-            key={l}
+            className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 hover:bg-dark-cyan hover:text-white"
+            onClick={() => props.selectedFilters(props.level)}
           >
-            {l}
+            {props.level}
           </button>
-        ))}
-
-        <button className="font-bold px-3 py-2 bg-light-grayish-cyan text-dark-cyan rounded mr-2 md:ml-2 hover:bg-dark-cyan hover:text-white">
-          {props.level}
-        </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
