d18017e259c84faa373bf9ab918c519754ab3d38
atripunoska <atripunoska@gmail.com>
2024-03-14 12:18:42 +0100

    feat: add header styling for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="flex justify-center text-lg">App</div>;
+  return (
+    <>
+      <header className="h-[150px] md:h-[200px] bg-[url('./src/assets/bg-header-mobile.svg')] md:bg-[url('./src/assets/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover bg-dark-cyan"></header>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-cyan": "hsl(180, 29%, 50%)",
+      },
+    },
   },
   plugins: [],
 };
