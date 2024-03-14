3452e50a699bd49fb15c1482677c49796f8b0dec
atripunoska <atripunoska@gmail.com>
2024-03-14 17:28:29 +0100

    refactor: update url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import JobListingCard from "./components/JobListingCard";
-import data from "./data.json";
+import data from "./assets/data.json";
 
 function App() {
   const [activeFilters, setActiveFilters] = useState<string[]>([]);
